b1002372c1262bd24c3035a0c0c9bd84f631ba02
Logan <98792682+LogansWebServices@users.noreply.github.com>
2022-03-26 05:41:06 -0400

    .

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/pern-post">
+    <BrowserRouter basename="/pern">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
